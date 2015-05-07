a383167cb7d9587ae6ed2c9b1fa1bbf047095ecd
NativeDark <nativedark@live.com>
2015-05-07 05:59:34 -0400

    initial API HIt

--- 1.py
+++ 1.py
@@ -1,6 +1,11 @@
 import sys
+import urllib2
 def main():
-  print 'test'
+  print 'getting all data'
+  file = urllib2.urlopen("http://api.openweathermap.org/data/2.5/weather?q=London&mode=xml")
+  data = file.read()
+  file.close()
+  print data
 
 if __name__ == '__main__':
   main()
