d9e7ecc7e1669e7240e3383db0f440b8519c87f5
NativeDark <nativedakr@live.com>
2015-05-11 03:01:48 -0400

    data

--- 1.py
+++ 1.py
@@ -3,17 +3,26 @@ import urllib2
 import xml.etree.ElementTree as ET
 def main():
   # print 'getting all data'
-  file = urllib2.urlopen("http://api.openweathermap.org/data/2.5/forecast?q=Delhi&mode=xml&cnt=16")
+  file = urllib2.urlopen("http://api.openweathermap.org/data/2.5/forecast/daily?q=Delhi&mode=xml&cnt=16")
   data = file.read()
   file.close()
   # print data
   root = ET.fromstring(data)
-  for dte in root.findall('location'):
-    print dte.find('name').text
-    print dte.find('country').text
-  for dte in root.iter('temperature'):
-    hold =  dte.attrib
-    print hold['min'], hold['max'], hold['value']
+  #for dte in root.findall('location'):
+  #  print dte.find('name').text
+  #  print dte.find('country').text
+  #for dte in root.iter('time'):
+  #  hold =  dte.attrib
+  #  print hold['day']
+  #print root[0][0].text
+  for child in root.findall('forecast'):
+    # print child
+    for sym in child.findall('time'):
+      for c2 in sym.findall('symbol'):
+        print c2.get('number'), c2.get('name'), sym.get('day')
+    # date = child.get('day').text
+    # print sym
+
 
 if __name__ == '__main__':
   main()
